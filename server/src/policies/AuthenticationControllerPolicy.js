667d34fa4515e04c6c81c399ad079f0f950914d6
Renan Pereira <pereira.renan@live.com>
2019-07-18 03:49:34 +0000

    part 2 done

---
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -0,0 +1,40 @@
+const Joi = require('joi')
+
+module.exports = {
+  register (req, res, next) {
+    const schema = {
+      email: Joi.string().email(),
+      password: Joi.string().regex(
+        new RegExp('^[a-zA-Z0-9]{8,32}$')
+      )
+    }
+
+    const { error } = Joi.validate(req.body, schema)
+
+    if (error) {
+      switch (error.details[0].context.key) {
+        case 'email':
+          res.status(400).send({
+            error: 'Please provide a valid email address.'
+          })
+          break
+        case 'password':
+          res.status(400).send({
+            error: `The password provided does not match the following rules:
+            <br>
+            1. It must contain ONLY: lower case, upper case and numerics.
+            <br>
+            2. It must be at least 8 characters in length and not greather than 32.
+          `
+          })
+          break
+        default:
+          res.status(400).send({
+            error: 'Invalid registration information'
+          })
+      }
+    } else {
+      next()
+    }
+  }
+}
