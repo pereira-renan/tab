bbe55de43f96052239f11f75e0555b864218769e
Renan Pereira <pereira.renan@live.com>
2019-07-18 04:44:09 +0000

    part 3 end of styling

---
+++ client/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-toolbar fixed class="cyan" dark>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class ="mr-4">
+        IBM Brasil - Customer Relationship Manager
+      </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat
+          @click="navigateTo({ name: 'register' })">
+            Signup
+        </v-btn>
+      <v-btn flat>Link Two</v-btn>
+      <v-btn flat>Link Three</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
